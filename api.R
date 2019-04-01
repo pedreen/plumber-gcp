66eb3f42f010a29acea5fbce9b2bda009b4b0aa5
pedreen <pedrop@gec.inatel.br>
2019-04-01 13:22:33 -0300

    Update api.R

--- api.R
+++ api.R
@@ -1,18 +1,24 @@
-#* Return the current time
-#* @get /teste/time
-function(){
-   Sys.time()
-}
-
+# plumber.R
+ 
 #* Echo back the input
-#* @param msg The first number to add
-#* @get /teste/echo
+#* @param msg The message to echo
+#* @get /echo
 function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
+    list(msg = paste0("The message is: '", msg, "'"))
 }
-
-#* Return the current system and user information
-#* @get /teste/info
+ 
+#* Plot a histogram
+#* @png
+#* @get /plot
 function(){
-   Sys.info()
+    rand <- rnorm(100)
+    hist(rand)
+}
+ 
+#* Return the sum of two numbers
+#* @param a The first number to add
+#* @param b The second number to add
+#* @post /sum
+function(a, b){
+    as.numeric(a) + as.numeric(b)
 }
